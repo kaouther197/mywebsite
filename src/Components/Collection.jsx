278616e579769cc1cc644ba1ebd3c47a030632fe
Brahim Kedjar <98288166+BrahimKedjar@users.noreply.github.com>
2022-10-03 14:29:29 +0100

    Update Collection.jsx

--- src/Components/Collection.jsx
+++ src/Components/Collection.jsx
@@ -1,45 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState,  } from "react";
 import "./Collection.css";
 import Modal from "./Modal";
 
 function Collection() {
-  const [active, setactive] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
 
-  useEffect(() => {
-    function scrollAnimation() {
-      const animationPoint = 300;
-      const windowHeight = window.innerHeight;
-      const collection = document.querySelector(".collection");
-      var collection_top = collection.getBoundingClientRect().top;
-
-      if (collection_top < windowHeight - animationPoint) {
-        setactive(true);
-      }
-    }
-
-    scrollAnimation();
-    window.addEventListener("scroll", scrollAnimation);
-  }, []);
+ 
  
   return (
-    <div className={`collection ${active && "active"}`} id="Collection">
+    <div className={`collection ${"active"}`} id="Collection">
       <h1>
         My <span>ULTIMATE</span> Home Work
       </h1>
       <div className="collections-container">
         <div className="cl-row row1">
           <div className="nft1" onClick={() => {
-            setModalOpen(true);
+            var result = prompt("Please enter password to show your tp:", " ");
+            if (result === "brahim1424") {
+              setModalOpen(true);
+
+            } else {
+              alert("incorrect");
+            }
+            
           }}></div>
           <div className="nft2" onClick={() => {
-            setModalOpen(true);
+            var result = prompt("Please enter password to show your tp:", " ");
+            if (result === "brahim1424") {
+              setModalOpen(true);
+
+            } else {
+              alert("incorrect");
+            }
           }}></div>
           <div className="nft3" onClick={() => {
-            setModalOpen(true);
+            var result = prompt("Please enter password to show your tp:", " ");
+            if (result === "brahim1424") {
+              setModalOpen(true);
+
+            } else {
+              alert("incorrect");
+            }
           }}></div>
           <div className="nft4" onClick={() => {
-            setModalOpen(true);
+            var result = prompt("Please enter password to show your tp:", " ");
+            if (result === "brahim1424") {
+              setModalOpen(true);
+
+            } else {
+              alert("incorrect");
+            }
           }}></div>
           
           {modalOpen && <Modal setOpenModal={setModalOpen} />}
